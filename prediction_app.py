63678e8bf4f6d74ea60a13307ae4e949d295e558
Nikhil Gundluru <nikhilgundluru349@gmail.com>
2024-07-25 11:40:47 +0530

    html change to root directory

--- prediction_app.py
+++ prediction_app.py
@@ -3,8 +3,8 @@ import pandas as pd
 import numpy as np
 import pickle
 
-# Initialize the Flask application
-app = Flask(__name__, template_folder='templates')
+# Initialize the Flask application with the current directory as the template folder
+app = Flask(__name__, template_folder='.')
 
 # Load the data and model
 data = pd.read_csv('cleaned_datset.csv')  # Ensure this file is in the correct location
@@ -31,7 +31,7 @@ def predict():
     balcony = request.form.get('balcony')
     sqft = request.form.get('sqft')
 
-    print(location,area_type, bhk, bath, sqft)
+    print(location, bhk, bath, sqft)
     
     # Create input data for the model
     input_data = pd.DataFrame([[location, area_type, float(bhk), float(bath), float(balcony), float(sqft)]], 
